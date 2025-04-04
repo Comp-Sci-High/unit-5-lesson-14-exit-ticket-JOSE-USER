85a7738b5647f8d0ccac1aaea0b63c42cc5b5e47
JOSE-USER <jose.corniel25@compscihigh.org>
2025-04-04 19:12:24 +0000

    Final Commit

--- public/script.js
+++ public/script.js
@@ -1,7 +1,27 @@
 // Create a function that tests the post route handler [2 pts]
 
+const base = "https://ominous-cod-7vpv6gr755vxcwqrj-3000.app.github.dev/"
+
+async function getPotion(){
+    const potionCreate = await fetch(base + "/create-potion", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            label: req.body.label,
+            effect: req.body.effect,
+            ingredients: req.body.ingredients,
+            color: req.body.color,
+            isExplosive: req.body.isExplosive 
+        }).save()
+    
+    })
+    const data = await potionCreate.json()
+}
 
 
 // Call the function [1 pt]
 // Show us the successful POST fetch [1 pt]
 
+getPotion()
